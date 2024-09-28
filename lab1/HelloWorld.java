6b9c0d562f27499d92c640035b3beb01e45cc957
tabetai1001 <145575191+tabetai1001@users.noreply.github.com>
2024-09-28 23:58:33 +0700

    Create HelloWorld.java

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,7 @@
+//Nguyen Hoang Tung 20225948
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Xin chao \n cac ban!");
+        System.out.println("Hello \n World!");
+    }
+}
